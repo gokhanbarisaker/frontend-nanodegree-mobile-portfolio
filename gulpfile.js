793cfc599ee40c412ff28a2c62015a3386db823e
Gokhan Baris Aker <gokhanbarisaker@gmail.com>
2015-04-27 17:22:12 +0400

    Minified css

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,13 @@ var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
 
 gulp.task('compress', function() {
-  return gulp.src('js/css/*.js')
+  return gulp.src('views/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('views/dist/js'));
 });
 
 gulp.task('css', function () {
-  gulp.src('./styles/**/*.css')
-    .pipe(uglifycss({
-      max-line-len: 80
-    }))
-    .pipe(gulp.dest('./dist/'));
+  gulp.src('views/css/*.css')
+    .pipe(uglifycss())
+    .pipe(gulp.dest('views/dist/css'));
 });

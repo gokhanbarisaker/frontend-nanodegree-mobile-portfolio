471f42222cd7ea92b9ccebc84197e97c62630054
Gokhan Baris Aker <gokhanbarisaker@gmail.com>
2015-04-30 00:45:01 +0400

    Added html minifier && fixed production paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,59 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
+var minifyhtml = require('gulp-minify-html');
+var merge = require('merge-stream');
 
 gulp.task('uglify-js', function() {
-  return gulp.src('development/views/js/*.js')
+  var pizza = gulp.src('development/views/js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('production/views/js'));
+
+  var root = gulp.src('developement/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('production/js'));
+
+  return merge(pizza, root);
 });
 
 gulp.task('uglify-css', function () {
-  gulp.src('development/views/css/*.css')
+  var pizza = gulp.src('development/views/css/*.css')
     .pipe(uglifycss())
     .pipe(gulp.dest('production/views/css'));
+
+  var root = gulp.src('development/css/*.css')
+    .pipe(uglifycss())
+    .pipe(gulp.dest('production/css'));
+
+  return merge(pizza, root);
 });
 
-gulp.task('uglify', ['uglify-js', 'uglify-css'], function () {
+gulp.task('minify-html', function() {
+  var options = {
+    conditionals: true,
+    spare:true
+  };
+
+  var pizza = gulp.src('development/views/pizza.html')
+    .pipe(minifyhtml(options))
+    .pipe(gulp.dest('production/views'));
+
+  var root = gulp.src('development/*.html')
+    .pipe(minifyhtml(options))
+    .pipe(gulp.dest('production'));
+
+  return merge(pizza, root);
+});
+
+gulp.task('image', function() {
+  var pizza = gulp.src('development/views/images/*.{png,jpg,jpeg,gif,svg}')
+    .pipe(gulp.dest('production/views/images'));
+
+  var root = gulp.src('development/img/*.{png,jpg,jpeg,gif,svg}')
+    .pipe(gulp.dest('production/img'));
+
+  return merge(pizza, root);
+});
+
+gulp.task('uglify', ['uglify-js', 'uglify-css', 'minify-html', 'image'], function () {
 });

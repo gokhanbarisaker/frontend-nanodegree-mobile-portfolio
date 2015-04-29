b7a12bb8354c934f31f5604d37e55462bdb4b68e
Gokhan Baris Aker <gokhanbarisaker@gmail.com>
2015-04-29 23:13:30 +0400

    Moved development files under development folder && Created productionfolder for production files && add gulp html minifier

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
 
-gulp.task('compress', function() {
+gulp.task('uglify-js', function() {
   return gulp.src('views/js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('views/dist/js'));
 });
 
-gulp.task('css', function () {
+gulp.task('uglify-css', function () {
   gulp.src('views/css/*.css')
     .pipe(uglifycss())
     .pipe(gulp.dest('views/dist/css'));
+});
+
+gulp.task('uglify', ['compress', 'css'], function () {
 });
